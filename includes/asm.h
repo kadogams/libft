8767eefc68228d5492e480a4462e4c8786db67db
Acacio Leclercqz <acacio@iMac-de-Acacio.local>
2019-02-08 18:17:17 +0100

    add name et add comment le add comment skip les whitespace a faire aussi pour le parse name

--- includes/asm.h
+++ includes/asm.h
@@ -3,3 +3,12 @@
 #include "../libftprintf/libft/includes/libft.h"
 #include "../libftprintf/libft/includes/get_next_line.h"
 #include <fcntl.h>
+
+typedef struct  s_asm
+{
+  int           fd_s;
+  int           fd_cor;
+}               t_asm;
+
+
+void	convert_to_hex(header_t *header, t_asm *env);
--- srcs_asm/srcs/convert_name_comment.c
+++ srcs_asm/srcs/convert_name_comment.c
@@ -8,15 +8,16 @@ static int	fill_prog_name(char *line, header_t *header, int i)
 	while (line[j] && i < PROG_NAME_LENGTH)
 	{
 		if (line[j] == '"')
-			return (check_)
-		str = ft_itoa_base(line[j], 16);
-		header->prog_name[i] = ft_strcpy(prog_name[i], str);
-		free(str);
+			return (0);
+		//str = ft_itoa_base(line[j], 16, 0);
+		header->prog_name[i] = line[j];
+	//	header->prog_name[i] = str[1];
+	//	free(str);
 		j++;
 		i++;
 	}
-	header->prog_name[i] = ft_strcpy(prog_name[i], "0a");
-	return (++i);
+	//header->prog_name = ft_strcpy(header->prog_name + i, "0a");
+	return (i);
 }
 
 static void	parse_name(header_t *header, t_asm *env)
@@ -27,20 +28,63 @@ static void	parse_name(header_t *header, t_asm *env)
 	char	*str;
 
 	i = 0;
+	ft_bzero(header->prog_name, PROG_NAME_LENGTH);
 	if ((ret = get_next_line(env->fd_s, &line)) > 0)
 	{
-		ft_printf("test\n");
-		if (!ft_strncmp(line, ".name", 5) && str = ft_strchr(line, '"'))
+		if (!ft_strncmp(line, ".name", 5) && (str = ft_strchr(line, '"')))
 		{
 			i = fill_prog_name(str + 1, header, 0);
-			while ((ret = get_next_line(env->fd_s, &line)) > 0)
+			while (i > 0 && (ret = get_next_line(env->fd_s, &line)) > 0)
 				i = fill_prog_name(line, header, i);
+			ft_printf("heaxa name : %s -- i = %d\n", header->prog_name, i);
 		}
 	}
 
+}
+int					is_whitespaces(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r'
+		|| str[i] == '\v' || str[i] == '\f')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+static void parse_comment(header_t *header, t_asm *env)
+{
+	int		ret;
+	char	*line;
+	int		i;
+	chqr *str;
+
+	i = 0;
+	ft_bzero(header->comment, COMMENT_LENGTH);
+	if ((ret = get_next_line(env->fd_s, &line)) > 0)
+	{
+		ft_printf("line comment = %s\n", line);
+		if (is_whitespaces(line))
+		{
+			ft_printf("ici");
+			parse_comment(header, env);
+		}
+		if (!ft_strncmp(line, ".comment", 5) && (str = ft_strchr(line, '"')))
+		{
+			i = fille_comment_name()
+			while (i > 0 && (ret = get_next_line(env->fd_s, &line)) > 0)
+				i = fille_comment_name()
+		}
+	}
 }
 
 void	convert_to_hex(header_t *header, t_asm *env)
 {
 	parse_name(header, env);
+	parse_comment(header, env);
 }
