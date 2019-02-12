728bf19c30b40b0779195bc6926eb4340654d656
Acacio Leclercqz <aclecler@e-r4-p7.s19.be>
2019-02-12 16:02:14 +0100

    dawdaw

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -17,6 +17,6 @@ typedef struct  s_asm
 int		init_env(t_asm *env);
 int		skip_whitespace(char *str, t_asm *env);
 int		check_last_line(char *line, int option, t_asm *env);
-void	skip_blank_lines(t_asm *env);
-void	start_parsing(header_t *header, t_asm *env);
+int		skip_blank_lines(t_asm *env);
+int 	start_parsing(header_t *header, t_asm *env);
 void	quit(t_asm *env);
--- asm/srcs/parsing.c
+++ asm/srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: dazheng <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 16:50:48 by dazheng           #+#    #+#             */
-/*   Updated: 2019/02/11 15:46:17 by dazheng          ###   ########.fr       */
+/*   Updated: 2019/02/12 14:57:18 by aclecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,12 +52,11 @@ int	parse_comment(header_t *header, t_asm *env)
 	int		i;
 
 	ft_bzero(header->comment, COMMENT_LENGTH);
-	skip_blank_lines(env);
-	if ((i = skip_whitespace(env->line, env)) == -1 &&
+	if (skip_blank_lines(env) && (i = skip_whitespace(env->line, env)) == -1 &&
 	ft_strncmp(env->line + i, ".comment", 8))
-		return (KO)
+		return (KO);
 	if	((ret = skip_whitespace(env->line + i + 8, env)) == -1)
-	 	return (KO)
+	 	return (KO);
 	i = ret + i + 8;
 	if (env->line[i] != '"')
 		return (KO);
@@ -78,9 +77,8 @@ int	parse_name(header_t *header, t_asm *env)
 	int		i;
 
 	ft_bzero(header->prog_name, PROG_NAME_LENGTH);
-	skip_blank_lines(env);
-	i = skip_whitespace(env->line, env);
-	if (ft_strncmp(env->line + i, ".name", 5))
+	if (!skip_blank_lines(env) && (i = skip_whitespace(env->line, env)) == -1
+	&& ft_strncmp(env->line + i, ".name", 5))
 		return (KO);
 	if ((ret = skip_whitespace(env->line + i + 5, env)) == -1)
 	 	return (KO);
@@ -95,7 +93,7 @@ int	parse_name(header_t *header, t_asm *env)
 			return (check_last_line(env->line, 0, env));
 		ft_strdel(&env->line);
 	}
-	return (KO)
+	return (KO);
 }
 
 int	start_parsing(header_t *header, t_asm *env)
@@ -103,7 +101,8 @@ int	start_parsing(header_t *header, t_asm *env)
 	if (!parse_name(header, env))
 		return (KO);
 	ft_printf("Prog name = |%s|", header->prog_name);
-	if (!parse_comment(header, env);)
+	if (!parse_comment(header, env))
 		return (KO);
 	ft_printf("comment = |%s|", header->comment);
+	return (OK);
 }
