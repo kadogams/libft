226ad37ad01cac9bcd0a919ff039735290588fa2
Shoei Kadogami <skadogam@e-r3-p4.s19.be>
2019-03-12 11:26:30 +0100

    aaa.s++

--- aaa.s
+++ aaa.s
@@ -7,11 +7,12 @@ trap:	st	r1,0
 st	r1,6
 
 a:		live	%0
-		st	r1,16
+		st	r1,13		#if line 13 is a comment
+#		st	r1,16		#if line 13 is not a comment
 		ld	%0,r2
-		fork	%:a
+#		fork	%:a
 
 b:		live	%0
-		fork	%:b
+#		fork	%:b
 		ld	%0,r2
 		zjmp	%:a
